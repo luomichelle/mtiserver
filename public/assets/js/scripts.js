1eaa1c08dc222623a686bdccaa0c196a8c66e008
Michelle Luo <Michelle Luo>
2017-04-15 23:34:53 -0700

    save

---
+++ public/assets/js/scripts.js
@@ -0,0 +1,76 @@
+
+jQuery(document).ready(function() {
+    /*
+        Login form validation
+    */
+    $('.login-form input[type="text"], .login-form input[type="password"], .login-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.login-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], input[type="password"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+    
+    /*
+        Registration form validation
+    */
+    $('.registration-form input[type="text"], .registration-form textarea').on('focus', function() {
+    	$(this).removeClass('input-error');
+    });
+    
+    $('.registration-form').on('submit', function(e) {
+    	
+    	$(this).find('input[type="text"], textarea').each(function(){
+    		if( $(this).val() == "" ) {
+    			e.preventDefault();
+    			$(this).addClass('input-error');
+    		}
+    		else {
+    			$(this).removeClass('input-error');
+    		}
+    	});
+    	
+    });
+});
+
+
+
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("watch-demo");
+var signup = document.getElementsByClassName("home-page-signup")[0];
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+    signup.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+    signup.style.display = "block";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+        signup.style.display = "block";
+    }
+}
--- server.js
+++ server.js
@@ -1,79 +1,76 @@
-/* Web Scraper Homework Solution Example
- *    (be sure to watch the video to see
- *     how to operate the site in the broser)
- * -/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/-/ */
-
-// Require our dependencies
 var express = require('express');
 var path = require('path');
-var mongoose = require('mongoose');
-var expressHandlebars = require('express-handlebars');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session'); 
+var methodOverride = require('method-override');
+var bparser = require('body-parser');
 
-// Instantiate our Express App
+
+
+// Model controllers (rather than routes)
+var application_controller = require('./controllers/application_controller');
+var character_controller = require('./controllers/character_controller');
+var user_controller = require('./controllers/user_controller');
+var truck_controller = require('./controllers/truck_controller');
+
+
+// Express settings
+
+// Instantiate the app
 var app = express();
 
-// Designate our public folder as a static directory
-app.use(express.static(__dirname + '/public'));
+// Override POST to have DELETE and PUT
+app.use(methodOverride('_method'))
 
-// connect Handlebars to our Express app
-app.engine('handlebars', expressHandlebars({
+// Allow sessions
+app.use(session({ secret: 'app', cookie: { maxAge: null }}));
+app.use(cookieParser());
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up handlebars
+var exphbs = require('express-handlebars');
+
+app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
+
 app.set('view engine', 'handlebars');
 
-// use bodyParser in our app
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', application_controller);
+app.use('/characters', character_controller);
+app.use('/users', user_controller);
+app.use('/trucks', truck_controller);
 
-// // Save MongoDB directory to a db var
-// var db = 'mongodb://localhost/mongoHeadlines';
-
-// // Connect that directory to Mongoose, for simple, powerful querying
-// mongoose.connect(db, function(err){
-// 	// log any errors connecting with mongoose
-//   if(err){
-//     console.log(err);
-//   } 
-//   // or log a success message
-//   else {
-//     console.log('mongoose connection is sucessful');
-//   }
-// });
-
-
-// MongoDB Configuration configuration (Change this URL to your own DB)
-mongoose.connect('mongodb://luomichelle:Xigua123456@ds151117.mlab.com:51117/mern');
-var db = mongoose.connection;
-
-db.on('error', function (err) {
-  console.log('Mongoose Error: ', err);
+// Catch 404 and forward to error handler
+app.use(function(req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
-db.once('open', function () {
-  console.log('Mongoose connection successful.');
+// Error handler
+// No stacktraces leaked to user unless in development environment
+app.use(function(err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: (app.get('env') === 'development') ? err : {}
+  	});
 });
 
 
-// bring in our routes file into the the server files
-var routes = require('./config/routes.js');
-
-// Incorporate these routes into our app
-app.use('/', routes);
-app.use('/test', routes);
-app.use('/fetch', routes);
-app.use('/gather', routes);
-app.use('/check', routes);
-app.use('/save', routes);
-app.use('/delete', routes);
+// our module get's exported as app.
+module.exports = app;
 
 
-// set up our port to be either the host's designated port, or 3000
-var port = process.env.PORT || 3000;
-
-// set our app to listen on the port.
-app.listen(port, function() {
-    console.log("lisenting on port:" + port);
-});
